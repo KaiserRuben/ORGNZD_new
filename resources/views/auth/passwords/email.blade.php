f3086bd6a378e0d012d733c510944cc7528ac9f6
Ruben Kaiser <C:\Users\Ruben\AppData\Roaming\The Bat!>
2018-03-10 15:58:20 +0100

    Reset-Password-send_email added design

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,60 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/resetMail.css') }}">
+	<title>ORGNZD. Reset password</title>
+</head>
+<body>
+	<div class="content"><br /><br />
+	<b class="name">
+		ORGNZD.
+	</b>
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<h1 class="description">
+		Reset Password?
+	</h1>
+	<br />
+  @if (session('status'))
+      <div class="alert alert-success">
+          {{ session('status') }}
+      </div>
+  @endif
+	<br />
+	<br />
+	<br /><br />
+	<br />
+  <form method="POST" action="{{ route('password.email') }}">
+      @csrf
+	<label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
+	<br />
+   <input type="text" id="email" size="30" maxlength="30" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+  <br />
+   @if ($errors->has('email'))
+       <span class="invalid-feedback">
+           <strong>{{ $errors->first('email') }}</strong>
+       </span>
+   @endif
+	<br />
+	<br />
+	<div class="contact">
+	<input type="Submit" class="button" type="submit" value="Send email" />
+  </form>
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<h4 class="noacc">
+	Remember your password? <a class="signup" href="/login">Go back.</a>
+	</h4>
 </div>
-@endsection
+</div>
+</body>
+</html>
